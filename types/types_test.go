c0cc0f579b0badb864fd1c2aaabdd2f27661307b
David Dollar <david@dollar.io>
2017-02-16 13:08:57 -0500

    wip

--- types/types_test.go
+++ types/types_test.go
@@ -1,1 +1,16 @@
 package types_test
+
+import (
+	"testing"
+
+	"github.com/convox/praxis/types"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestId(t *testing.T) {
+	id := types.Id("A", 10)
+
+	assert.Equal(t, "A", id[0:1])
+	assert.Len(t, id, 10)
+}
