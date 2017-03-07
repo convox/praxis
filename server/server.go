c8ebe20f2cb44317de0f9e57ad19ecc7d9e22f6f
David Dollar <david@dollar.io>
2017-03-07 15:15:52 -0600

    remove debug

--- server/server.go
+++ server/server.go
@@ -1,11 +1,6 @@
 package server
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/convox/api"
-)
+import "github.com/convox/api"
 
 type Server struct {
 	*api.Server
@@ -14,26 +9,7 @@ type Server struct {
 func New() *Server {
 	server := api.New("rack", "convox.rack")
 
-	server.UseHandlerFunc(mw1)
-
 	Routes(server)
 
 	return &Server{Server: server}
-}
-
-func mw1(w http.ResponseWriter, r *http.Request) {
-	// w.Write([]byte("bar"))
-	fmt.Println("mw1")
-}
-
-func mw2(fn api.HandlerFunc) api.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-		fmt.Println("mw2 before")
-		// return fmt.Errorf("whaaa")
-		if err := fn(w, r, c); err != nil {
-			return err
-		}
-		fmt.Println("mw2 after")
-		return nil
-	}
 }
