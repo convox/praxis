378e34d026037b999a48f1f9c0f079ba10d99d77
Miguel Moll <miguel@convox.com>
2017-03-09 12:46:58 -0600

    Build controller tests

---
+++ server/controllers/build_test.go
@@ -0,0 +1,103 @@
+package controllers_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/url"
+	"testing"
+
+	"github.com/convox/praxis/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildCreate(t *testing.T) {
+	ts, mp := mockServer()
+	defer ts.Close()
+
+	build := &types.Build{
+		Id:     "BTEST",
+		App:    "app",
+		Status: "created",
+	}
+	opts := types.BuildCreateOptions{Cache: true}
+	mp.On("BuildCreate", "app", "http://example.com", opts).Return(build, nil)
+
+	v := url.Values{}
+	v.Add("url", "http://example.com")
+	v.Add("cache", "true")
+
+	res, err := testRequest(ts, "POST", "/apps/app/builds", bytes.NewReader([]byte(v.Encode())))
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t,
+		[]byte(`{"id":"BTEST","app":"app","manifest":"","process":"","release":"","status":"created","started":"0001-01-01T00:00:00Z","ended":"0001-01-01T00:00:00Z"}`),
+		data,
+	)
+}
+
+func TestBuildGet(t *testing.T) {
+	ts, mp := mockServer()
+	defer ts.Close()
+
+	build := &types.Build{
+		Id:     "BTEST",
+		App:    "app",
+		Status: "created",
+	}
+	mp.On("BuildGet", "app", "BTEST").Return(build, nil)
+
+	res, err := testRequest(ts, "GET", "/apps/app/builds/BTEST", nil)
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t,
+		[]byte(`{"id":"BTEST","app":"app","manifest":"","process":"","release":"","status":"created","started":"0001-01-01T00:00:00Z","ended":"0001-01-01T00:00:00Z"}`),
+		data,
+	)
+}
+
+func TestBuildUpdate(t *testing.T) {
+	ts, mp := mockServer()
+	defer ts.Close()
+
+	build := &types.Build{
+		Id:      "BTEST",
+		App:     "app",
+		Status:  "pending",
+		Release: "RTEST",
+	}
+
+	opts := types.BuildUpdateOptions{
+		Manifest: `{"manifest": "foo"}`,
+		Release:  "RTEST",
+		Status:   "pending",
+	}
+	mp.On("BuildUpdate", "app", "BTEST", opts).Return(build, nil)
+
+	v := url.Values{}
+	v.Add("manifest", `{"manifest": "foo"}`)
+	v.Add("release", "RTEST")
+	v.Add("status", "pending")
+
+	res, err := testRequest(ts, "PUT", "/apps/app/builds/BTEST", bytes.NewReader([]byte(v.Encode())))
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t,
+		[]byte(`{"id":"BTEST","app":"app","manifest":"","process":"","release":"RTEST","status":"pending","started":"0001-01-01T00:00:00Z","ended":"0001-01-01T00:00:00Z"}`),
+		data,
+	)
+}
