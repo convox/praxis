814324cde75110f0d7d1cf80c089bef83d8c3cc4
Miguel Moll <miguel@convox.com>
2017-03-09 19:53:58 -0600

    table controller tests

--- server/controllers/table_test.go
+++ server/controllers/table_test.go
@@ -2,6 +2,7 @@ package controllers_test
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/url"
 	"testing"
@@ -9,6 +10,37 @@ import (
 	"github.com/convox/praxis/types"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestTableCreate(t *testing.T) {
+	ts, mp := mockServer()
+	defer ts.Close()
+
+	mp.On("TableCreate", "app", "table1", types.TableCreateOptions{Indexes: []string{"floor"}}).Return(nil)
+
+	v := url.Values{}
+	v.Add("index", "floor")
+
+	res, err := testRequest(ts, "POST", "/apps/app/tables/table1", bytes.NewReader([]byte(v.Encode())))
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t, []byte(``), data)
+
+	mp.On("TableCreate", "app", "tableerror", types.TableCreateOptions{Indexes: []string{"floor"}}).Return(fmt.Errorf("failed to create table"))
+	reserr, err := testRequest(ts, "POST", "/apps/app/tables/tableerror", bytes.NewReader([]byte(v.Encode())))
+	assert.NoError(t, err)
+	defer reserr.Body.Close()
+
+	data, err = ioutil.ReadAll(reserr.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 500, reserr.StatusCode)
+	assert.Equal(t, []byte("failed to create table\n"), data)
+}
 
 func TestTableFetchBatch(t *testing.T) {
 	ts, mp := mockServer()
@@ -34,4 +66,48 @@ func TestTableFetchBatch(t *testing.T) {
 
 	assert.Equal(t, 200, res.StatusCode)
 	assert.Equal(t, []byte(`[{"foo":"bar1","id":"one"},{"foo":"bar2","id":"two"}]`), data)
+}
+
+func TestTableGet(t *testing.T) {
+	ts, mp := mockServer()
+	defer ts.Close()
+
+	table := &types.Table{
+		Name:    "table",
+		Indexes: []string{"foo", "baz"},
+	}
+	mp.On("TableGet", "app", "table").Return(table, nil)
+
+	res, err := testRequest(ts, "GET", "/apps/app/tables/table", nil)
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t, []byte(`{"Name":"table","Indexes":["foo","baz"]}`), data)
+}
+
+func TestTableList(t *testing.T) {
+	ts, mp := mockServer()
+	defer ts.Close()
+
+	tables := types.Tables{
+		{Name: "table", Indexes: []string{"foo", "baz"}},
+		{Name: "table2", Indexes: []string{"baz"}},
+		{Name: "table1", Indexes: []string{"floor"}},
+	}
+
+	mp.On("TableList", "app").Return(tables, nil)
+
+	res, err := testRequest(ts, "GET", "/apps/app/tables", nil)
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t, []byte(`[{"Name":"table","Indexes":["foo","baz"]},{"Name":"table2","Indexes":["baz"]},{"Name":"table1","Indexes":["floor"]}]`), data)
 }
