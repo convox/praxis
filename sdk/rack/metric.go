f59cb70be24b30bb70b37e7e02b5b6c2cfe5ec78
Noah Zoschke <noah@convox.com>
2017-03-23 13:19:43 -0700

    MetricGet client, route and handler

--- sdk/rack/metric.go
+++ sdk/rack/metric.go
@@ -6,12 +6,14 @@ import (
 	"github.com/convox/praxis/types"
 )
 
+func (c *Client) MetricGet(app, namespace, metric string, opts types.MetricGetOptions) ([]string, error) {
+	metrics := []string{}
+	err := c.Get(fmt.Sprintf("/apps/%s/metrics/%s/%s", app, namespace, metric), RequestOptions{}, &metrics)
+	return metrics, err
+}
+
 func (c *Client) MetricList(app, namespace string, opts types.MetricListOptions) ([]string, error) {
 	metrics := []string{}
 	err := c.Get(fmt.Sprintf("/apps/%s/metrics/%s", app, namespace), RequestOptions{}, &metrics)
 	return metrics, err
-}
-
-func (c *Client) MetricGet(app, namespace, metric string, opts types.MetricGetOptions) ([]string, error) {
-	return []string{}, nil
 }
--- server/controllers/metric.go
+++ server/controllers/metric.go
@@ -7,6 +7,19 @@ import (
 	"github.com/convox/praxis/types"
 )
 
+func MetricGet(w http.ResponseWriter, r *http.Request, c *api.Context) error {
+	app := c.Var("app")
+	ns := c.Var("namespace")
+	m := c.Var("metric")
+
+	metrics, err := Provider.MetricGet(app, ns, m, types.MetricGetOptions{})
+	if err != nil {
+		return err
+	}
+
+	return c.RenderJSON(metrics)
+}
+
 func MetricList(w http.ResponseWriter, r *http.Request, c *api.Context) error {
 	app := c.Var("app")
 	ns := c.Var("namespace")
