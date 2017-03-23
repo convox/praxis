b5ecd6eb4a6ad06dde2b72262fecc729d43d1ba9
Noah Zoschke <noah@convox.com>
2017-03-23 12:30:11 -0700

    cx metrics boilerplate

---
+++ cmd/cx/metrics.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/convox/praxis/stdcli"
+	"github.com/convox/praxis/types"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+func init() {
+	stdcli.RegisterCommand(cli.Command{
+		Name:        "metrics",
+		Description: "show app metrics",
+		Flags: []cli.Flag{
+			appFlag,
+		},
+		Subcommands: []cli.Command{
+			cli.Command{
+				Name:        "list",
+				Description: "list metrics",
+				Usage:       "<namespace>",
+				Action:      listMetrics,
+			},
+			cli.Command{
+				Name:        "get",
+				Description: "display metrics",
+				Usage:       "<namespace> <metric>",
+				Action:      getMetrics,
+			},
+		},
+	})
+}
+
+func listMetrics(c *cli.Context) error {
+	app, err := appName(c, ".")
+	if err != nil {
+		return err
+	}
+
+	if c.NArg() != 1 {
+		return stdcli.Usage(c)
+	}
+
+	ns := c.Args()[0]
+
+	metrics, err := Rack.MetricList(app, ns, types.MetricListOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("METRICS: %+v\n", metrics)
+	return nil
+}
+
+func getMetrics(c *cli.Context) error {
+	app, err := appName(c, ".")
+	if err != nil {
+		return err
+	}
+
+	if c.NArg() != 2 {
+		return stdcli.Usage(c)
+	}
+
+	ns := c.Args()[0]
+	m := c.Args()[1]
+
+	metrics, err := Rack.MetricGet(app, ns, m, types.MetricGetOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("METRICS: %+v\n", metrics)
+	return nil
+}
