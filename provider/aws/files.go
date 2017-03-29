79034c394a5dc052a72ef86d6e4aacfba8cd036d
David Dollar <david@dollar.io>
2017-03-29 18:32:32 -0400

    mark unimplemented aws provider methods

--- provider/aws/files.go
+++ provider/aws/files.go
@@ -1,13 +1,14 @@
 package aws
 
 import (
+	"fmt"
 	"io"
 )
 
 func (p *Provider) FilesDelete(app, pid string, files []string) error {
-	return nil
+	return fmt.Errorf("unimplemented")
 }
 
 func (p *Provider) FilesUpload(app, pid string, r io.Reader) error {
-	return nil
+	return fmt.Errorf("unimplemented")
 }
