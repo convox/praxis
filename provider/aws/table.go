ff3059886ea0437551f7e3b466c3940aa0d97667
David Dollar <david@dollar.io>
2017-05-04 13:08:09 -0400

    clean up table api

--- provider/aws/table.go
+++ provider/aws/table.go
@@ -18,26 +18,10 @@ func (p *Provider) TableList(app string) (types.Tables, error) {
 	return nil, fmt.Errorf("unimplemented")
 }
 
+func (p *Provider) TableQuery(app, table, query string) (types.TableRows, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+
 func (p *Provider) TableTruncate(app, table string) error {
 	return fmt.Errorf("unimplemented")
-}
-
-func (p *Provider) TableRowDelete(app, table, key string, opts types.TableRowDeleteOptions) error {
-	return fmt.Errorf("unimplemented")
-}
-
-func (p *Provider) TableRowGet(app, table, key string, opts types.TableRowGetOptions) (*types.TableRow, error) {
-	return nil, fmt.Errorf("unimplemented")
-}
-
-func (p *Provider) TableRowStore(app, table string, attrs types.TableRow) (string, error) {
-	return "", fmt.Errorf("unimplemented")
-}
-
-func (p *Provider) TableRowsDelete(app, table string, keys []string, opts types.TableRowDeleteOptions) error {
-	return fmt.Errorf("unimplemented")
-}
-
-func (p *Provider) TableRowsGet(app, table string, keys []string, opts types.TableRowGetOptions) (types.TableRows, error) {
-	return nil, fmt.Errorf("unimplemented")
 }
--- server/controllers/table.go
+++ server/controllers/table.go
@@ -5,20 +5,7 @@ import (
 	"sort"
 
 	"github.com/convox/api"
-	"github.com/convox/praxis/types"
 )
-
-func TableCreate(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-	if err := r.ParseForm(); err != nil {
-		return err
-	}
-
-	app := c.Var("app")
-	table := c.Var("table")
-	indexes := r.Form["index"]
-
-	return Provider.TableCreate(app, table, types.TableCreateOptions{Indexes: indexes})
-}
 
 func TableGet(w http.ResponseWriter, r *http.Request, c *api.Context) error {
 	app := c.Var("app")
@@ -43,82 +30,6 @@ func TableList(w http.ResponseWriter, r *http.Request, c *api.Context) error {
 	sort.Slice(t, t.Less)
 
 	return c.RenderJSON(t)
-}
-
-func TableRowDelete(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-	app := c.Var("app")
-	table := c.Var("table")
-	index := c.Var("index")
-	key := c.Var("key")
-
-	return Provider.TableRowDelete(app, table, key, types.TableRowDeleteOptions{Index: index})
-}
-
-func TableRowGet(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-	app := c.Var("app")
-	table := c.Var("table")
-	index := c.Var("index")
-	key := c.Var("key")
-
-	attrs, err := Provider.TableRowGet(app, table, key, types.TableRowGetOptions{Index: index})
-	if err != nil {
-		return err
-	}
-
-	return c.RenderJSON(attrs)
-}
-
-func TableRowStore(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-	app := c.Var("app")
-	table := c.Var("table")
-
-	if err := r.ParseForm(); err != nil {
-		return err
-	}
-
-	attrs := map[string]string{}
-
-	for k := range r.Form {
-		attrs[k] = r.Form.Get(k)
-	}
-
-	id, err := Provider.TableRowStore(app, table, attrs)
-	if err != nil {
-		return err
-	}
-
-	return c.RenderJSON(id)
-}
-
-func TableRowsDelete(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-	if err := r.ParseForm(); err != nil {
-		return err
-	}
-
-	app := c.Var("app")
-	table := c.Var("table")
-	index := c.Var("index")
-	keys := r.Form["key"]
-
-	return Provider.TableRowsDelete(app, table, keys, types.TableRowDeleteOptions{Index: index})
-}
-
-func TableRowsGet(w http.ResponseWriter, r *http.Request, c *api.Context) error {
-	if err := r.ParseForm(); err != nil {
-		return err
-	}
-
-	app := c.Var("app")
-	table := c.Var("table")
-	index := c.Var("index")
-	keys := r.Form["key"]
-
-	items, err := Provider.TableRowsGet(app, table, keys, types.TableRowGetOptions{Index: index})
-	if err != nil {
-		return err
-	}
-
-	return c.RenderJSON(items)
 }
 
 func TableTruncate(w http.ResponseWriter, r *http.Request, c *api.Context) error {
