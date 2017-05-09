c7c8e4e864b5e40c36630cbc09ba53798ffecf43
David Dollar <david@dollar.io>
2017-05-09 15:52:03 -0400

    resource list

--- provider/local/resource.go
+++ provider/local/resource.go
@@ -2,10 +2,55 @@ package local
 
 import (
 	"fmt"
+	"sort"
 
+	"github.com/convox/praxis/manifest"
 	"github.com/convox/praxis/types"
 )
 
 func (p *Provider) ResourceList(app string) (types.Resources, error) {
-	return nil, fmt.Errorf("unimplemented")
+	a, err := p.AppGet(app)
+	if err != nil {
+		return nil, err
+	}
+	if a.Release == "" {
+		return nil, fmt.Errorf("no release for app: %s\n", app)
+	}
+
+	r, err := p.ReleaseGet(app, a.Release)
+	if err != nil {
+		return nil, err
+	}
+	if r.Build == "" {
+		return nil, fmt.Errorf("no build for app: %s\n", app)
+	}
+
+	b, err := p.BuildGet(app, r.Build)
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := manifest.Load([]byte(b.Manifest))
+	if err != nil {
+		return nil, err
+	}
+
+	rs := make(types.Resources, len(m.Resources))
+
+	for i, r := range m.Resources {
+		e, err := resourceURL(a.Name, r.Type, r.Name)
+		if err != nil {
+			return nil, err
+		}
+
+		rs[i] = types.Resource{
+			Name:     r.Name,
+			Endpoint: e,
+			Type:     r.Type,
+		}
+	}
+
+	sort.Slice(rs, func(i, j int) bool { return rs[i].Name < rs[j].Name })
+
+	return rs, nil
 }
