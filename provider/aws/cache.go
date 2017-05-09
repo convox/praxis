360af775e2736bc3d9c8053d59b7928340cab966
Miguel Moll <miguel@convox.com>
2017-05-09 18:13:41 -0400

    cache controller and sdk impl

--- provider/aws/cache.go
+++ provider/aws/cache.go
@@ -8,8 +8,8 @@ import (
 
 var cacheBuffer = make(map[string]interface{})
 
-func (p *Provider) CacheFetch(app, cache, key string) map[string]string {
-	return nil
+func (p *Provider) CacheFetch(app, cache, key string) (map[string]string, error) {
+	return nil, nil
 }
 
 func (p *Provider) CacheStore(app, cache, key string, attrs map[string]string, opts types.CacheStoreOptions) error {
--- sdk/rack/cache.go
+++ sdk/rack/cache.go
@@ -6,10 +6,21 @@ import (
 	"github.com/convox/praxis/types"
 )
 
-func (c *Client) CacheFetch(app, cache, key string) map[string]string {
-	return nil
+func (c *Client) CacheFetch(app, cache, key string) (attrs map[string]string, err error) {
+	err = c.Get(fmt.Sprintf("/apps/%s/caches/%s/%s", app, cache, key), RequestOptions{}, &attrs)
+	return
 }
 
 func (c *Client) CacheStore(app, cache, key string, attrs map[string]string, opts types.CacheStoreOptions) error {
-	return fmt.Errorf("unimplemented")
+	params := map[string]interface{}{}
+
+	for k, v := range attrs {
+		params[k] = v
+	}
+
+	ro := RequestOptions{
+		Params: params,
+	}
+
+	return c.Post(fmt.Sprintf("/apps/%s/caches/%s/%s", app, cache, key), ro, nil)
 }
