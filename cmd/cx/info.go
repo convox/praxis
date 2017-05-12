9b2bab91c31636f132afaed0eb6d5cd15483ab5d
David Dollar <david@dollar.io>
2017-05-12 01:16:13 -0400

    fix info

---
+++ cmd/cx/info.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/convox/praxis/stdcli"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+func init() {
+	stdcli.RegisterCommand(cli.Command{
+		Name:        "info",
+		Description: "get application info",
+		Action:      runAppsInfo,
+		Flags:       []cli.Flag{appFlag},
+	})
+}
