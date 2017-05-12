1dc24044cc044a11eb4f6b4fade7f0362be15683
David Dollar <david@dollar.io>
2017-05-12 00:43:57 -0400

    delete bucket after stack delete

--- provider/aws/app.go
+++ provider/aws/app.go
@@ -44,17 +44,19 @@ func (p *Provider) AppCreate(name string) (*types.App, error) {
 }
 
 func (p *Provider) AppDelete(name string) error {
-	if bucket, err := p.appResource(name, "Bucket"); err == nil && bucket != "" {
-		if err := p.deleteBucket(bucket); err != nil {
-			fmt.Println("ns=provider.aws at=app.delete error=%q\n", err)
-		}
-	}
+	bucket, _ := p.appResource(name, "Bucket")
 
 	_, err := p.CloudFormation().DeleteStack(&cloudformation.DeleteStackInput{
 		StackName: aws.String(fmt.Sprintf("%s-%s", p.Name, name)),
 	})
 	if err != nil {
 		return err
+	}
+
+	if bucket != "" {
+		if err := p.deleteBucket(bucket); err != nil {
+			fmt.Println("ns=provider.aws at=app.delete error=%q\n", err)
+		}
 	}
 
 	return nil
