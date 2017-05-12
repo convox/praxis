a755a7ee5fbd3920ad54ab5d0a1306750ff368fe
David Dollar <david@dollar.io>
2017-05-12 00:43:21 -0400

    add http redirect to rack balancer

--- cmd/rack/main.go
+++ cmd/rack/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/convox/praxis/server"
 )
@@ -14,6 +16,15 @@ func main() {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
 		os.Exit(1)
 	}
+
+	go http.ListenAndServe(":3001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u := r.URL
+
+		u.Host = strings.Split(r.Host, ":")[0]
+		u.Scheme = "https"
+
+		http.Redirect(w, r, u.String(), http.StatusTemporaryRedirect)
+	}))
 
 	if err := s.Listen("tcp", ":3000"); err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
