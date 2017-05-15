c310870c700a83ab5733531208f6fe6c15b2967f
David Dollar <david@dollar.io>
2017-05-15 14:43:24 -0400

    tick workers can get backed up and spin the cpu

--- provider/local/workers.go
+++ provider/local/workers.go
@@ -12,20 +12,22 @@ func (p *Provider) Workers() {
 	terminate := make(chan os.Signal, 1)
 	signal.Notify(terminate, syscall.SIGINT, syscall.SIGTERM)
 
-	converge := time.Tick(5 * time.Second)
+	go func() {
+		<-terminate
+		if err := p.shutdown(); err != nil {
+			fmt.Printf("ns=provider.local at=shutdown error=%q\n", err)
+		}
+	}()
 
-	for {
-		select {
-		case <-terminate:
-			if err := p.shutdown(); err != nil {
-				fmt.Printf("ns=provider.local at=shutdown error=%q\n", err)
-			}
-		case <-converge:
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+
 			if err := p.workerConverge(); err != nil {
 				fmt.Printf("ns=provider.local at=converge error=%q\n", err)
 			}
 		}
-	}
+	}()
 }
 
 func (p *Provider) workerConverge() error {
