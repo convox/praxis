df98f468399bb0cf8cd7da377242e2537dad6104
Miguel Moll <miguel@convox.com>
2017-05-16 18:37:27 -0400

    appOutput has it's own cache

--- provider/aws/cache.go
+++ provider/aws/cache.go
@@ -2,9 +2,7 @@ package aws
 
 import (
 	"fmt"
-	"time"
 
-	lcache "github.com/convox/praxis/cache"
 	"github.com/convox/praxis/types"
 	"github.com/go-redis/redis"
 )
@@ -33,33 +31,23 @@ func (p *Provider) CacheStore(app, cache, key string, attrs map[string]string, o
 }
 
 func (p *Provider) redisClient(app, cache string) (*redis.Client, error) {
-	ep := lcache.Get("redis_endpoints", fmt.Sprintf("%s:%s", app, cache))
-	if ep == nil {
-		re, err := p.appOutput(app, fmt.Sprintf("Cache%sEndpoint", upperName(cache)))
-		if err != nil {
-			return nil, fmt.Errorf("redis endpoint: %s", err)
-		}
+	re, err := p.appOutput(app, fmt.Sprintf("Cache%sEndpoint", upperName(cache)))
+	if err != nil {
+		return nil, fmt.Errorf("redis endpoint: %s", err)
+	}
 
-		rp, err := p.appOutput(app, fmt.Sprintf("Cache%sPort", upperName(cache)))
-		if err != nil {
-			return nil, fmt.Errorf("redis port: %s", err)
-		}
-
-		ep = fmt.Sprintf("%s:%s", re, rp)
-
-		err = lcache.Set("redis_endpoints", fmt.Sprintf("%s:%s", app, cache), ep, 1*time.Hour)
-		if err != nil {
-			fmt.Printf("redis endpoint not cached: %s", ep)
-		}
+	rp, err := p.appOutput(app, fmt.Sprintf("Cache%sPort", upperName(cache)))
+	if err != nil {
+		return nil, fmt.Errorf("redis port: %s", err)
 	}
 
 	client := redis.NewClient(&redis.Options{
-		Addr:     ep.(string),
+		Addr:     fmt.Sprintf("%s:%s", re, rp),
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	_, err := client.Ping().Result()
+	_, err = client.Ping().Result()
 	if err != nil {
 		return nil, fmt.Errorf("redis pong: %s", err)
 	}
