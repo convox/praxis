35dbfa2370d97acd27e888e52aa66c19a71a23ac
Matthew Manning <matt.manning@gmail.com>
2017-06-01 18:41:27 -0400

    applyDefaults -> ApplyDefaults to export it

--- manifest/manifest.go
+++ manifest/manifest.go
@@ -41,7 +41,7 @@ func Load(data []byte, env Environment) (*Manifest, error) {
 
 	m.Environment = env
 
-	if err := m.applyDefaults(); err != nil {
+	if err := m.ApplyDefaults(); err != nil {
 		return nil, err
 	}
 
@@ -113,7 +113,7 @@ func (m *Manifest) Validate() error {
 	return nil
 }
 
-func (m *Manifest) applyDefaults() error {
+func (m *Manifest) ApplyDefaults() error {
 	for i, s := range m.Services {
 		if s.Build.Path == "" && s.Image == "" {
 			m.Services[i].Build.Path = "."
