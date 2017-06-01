0d084038bf1bb431b96de262c68ca64d0d324d14
David Dollar <david@dollar.io>
2017-06-01 13:56:22 -0400

    logging

--- provider/local/key.go
+++ provider/local/key.go
@@ -4,6 +4,8 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/hex"
+
+	"github.com/pkg/errors"
 )
 
 const (
@@ -12,39 +14,61 @@ const (
 )
 
 func (p *Provider) KeyDecrypt(app, key string, data []byte) ([]byte, error) {
+	log := p.logger("KeyDecrypt").Append("app=%q key=%q", app, key)
+
+	if _, err := p.AppGet(app); err != nil {
+		return nil, log.Error(err)
+	}
+
 	block, err := aes.NewCipher([]byte(aesKey))
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(log.Error(err))
 	}
 
 	nonce, err := hex.DecodeString(nonceHex)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(log.Error(err))
 	}
 
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(log.Error(err))
 	}
 
-	return aesgcm.Open(nil, nonce, data, nil)
+	dec, err := aesgcm.Open(nil, nonce, data, nil)
+	if err != nil {
+		return nil, errors.WithStack(log.Error(err))
+	}
+
+	return dec, log.Success()
 }
 
 func (p *Provider) KeyEncrypt(app, key string, data []byte) ([]byte, error) {
+	log := p.logger("KeyEncrypt").Append("app=%q key=%q", app, key)
+
+	if _, err := p.AppGet(app); err != nil {
+		return nil, log.Error(err)
+	}
+
 	block, err := aes.NewCipher([]byte(aesKey))
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(log.Error(err))
 	}
 
 	nonce, err := hex.DecodeString(nonceHex)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(log.Error(err))
 	}
 
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(log.Error(err))
 	}
 
-	return aesgcm.Seal(nil, nonce, data, nil), nil
+	enc, err := aesgcm.Seal(nil, nonce, data, nil), nil
+	if err != nil {
+		return nil, errors.WithStack(log.Error(err))
+	}
+
+	return enc, log.Success()
 }
--- provider/local/queue.go
+++ provider/local/queue.go
@@ -7,38 +7,59 @@ import (
 
 	"github.com/boltdb/bolt"
 	"github.com/convox/praxis/types"
+	"github.com/pkg/errors"
 )
 
 func (p *Provider) QueueFetch(app, queue string, opts types.QueueFetchOptions) (map[string]string, error) {
+	log := p.logger("QueueFetch").Append("app=%q queue=%q", app, queue)
+
 	tick := time.Tick(200 * time.Millisecond)
 	timeout := time.After(time.Duration(coalescei(opts.Timeout, 10)) * time.Second)
+
+	if _, err := p.AppGet(app); err != nil {
+		return nil, log.Error(err)
+	}
 
 	for {
 		select {
 		case <-tick:
 			item, err := p.queueFetchItem(app, queue)
 			if err != nil {
-				return nil, err
+				return nil, errors.WithStack(log.Error(err))
 			}
 			if item != nil {
-				return item, nil
+				return item, log.Success()
 			}
 		case <-timeout:
-			return nil, nil
+			return nil, log.Successf("timeout=true")
 		}
 	}
 }
 
 func (p *Provider) QueueStore(app, queue string, attrs map[string]string) error {
-	data, err := json.Marshal(attrs)
-	if err != nil {
-		return err
+	log := p.logger("QueueStore").Append("app=%q queue=%q", app, queue)
+
+	if _, err := p.AppGet(app); err != nil {
+		return log.Error(err)
 	}
 
-	return p.storageLogWrite(fmt.Sprintf("apps/%s/queues/%s", app, queue), data)
+	data, err := json.Marshal(attrs)
+	if err != nil {
+		return errors.WithStack(log.Error(err))
+	}
+
+	if err := p.storageLogWrite(fmt.Sprintf("apps/%s/queues/%s", app, queue), data); err != nil {
+		return errors.WithStack(log.Error(err))
+	}
+
+	return log.Success()
 }
 
 func (p *Provider) queueFetchItem(app, queue string) (map[string]string, error) {
+	if _, err := p.AppGet(app); err != nil {
+		return nil, err
+	}
+
 	var item map[string]string
 
 	err := p.storageBucket(fmt.Sprintf("apps/%s/queues/%s", app, queue), func(bucket *bolt.Bucket) error {
