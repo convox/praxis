187c1b68a0836729d013f98a06a2ed739ac92564
Matthew Manning <matt.manning@gmail.com>
2017-06-06 01:29:59 -0400

    service yaml tags

--- manifest/service.go
+++ manifest/service.go
@@ -8,23 +8,23 @@ import (
 type Service struct {
 	Name string
 
-	Build       ServiceBuild
-	Certificate string
-	Command     ServiceCommand
-	Environment []string
-	Health      ServiceHealth
-	Image       string
-	Port        ServicePort
-	Resources   []string
-	Scale       ServiceScale
-	Volumes     []string
+	Build       ServiceBuild   `yaml:"build,omitempty"`
+	Certificate string         `yaml:"certificate,omitempty"`
+	Command     ServiceCommand `yaml:"command,omitempty"`
+	Environment []string       `yaml:"environment,omitempty"`
+	Health      ServiceHealth  `yaml:"health,omitempty"`
+	Image       string         `yaml:"image,omitempty"`
+	Port        ServicePort    `yaml:"port,omitempty"`
+	Resources   []string       `yaml:"resources,omitempty"`
+	Scale       ServiceScale   `yaml:"scale,omitempty"`
+	Volumes     []string       `yaml:"volumes,omitempty"`
 }
 
 type Services []Service
 
 type ServiceBuild struct {
-	Args []string
-	Path string
+	Args []string `yaml:"args,omitempty"`
+	Path string   `yaml:"path,omitempty"`
 }
 
 type ServiceCommand struct {
