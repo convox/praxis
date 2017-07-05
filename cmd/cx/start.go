e07f978812ba07b688f7c959233b746abafc1165
Noah Zoschke <noah@convox.com>
2017-07-05 13:08:10 -0700

    fix vet error

--- cmd/cx/start.go
+++ cmd/cx/start.go
@@ -170,10 +170,10 @@ func handleSignals(r rack.Rack, ch chan os.Signal, errch chan error, m *manifest
 	for _, p := range ps {
 		m.Writef("convox", "stopping %s\n", p.Id)
 
-		go func() {
+		go func(id string) {
 			defer wg.Done()
-			r.ProcessStop(app, p.Id)
-		}()
+			r.ProcessStop(app, id)
+		}(p.Id)
 	}
 
 	wg.Wait()
