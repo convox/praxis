c5051d54ed75d7f62d1fdacf698b756380fb9baa
Noah Zoschke <noah@convox.com>
2017-07-06 11:23:01 -0700

    clear cache so cx test doesnt get "no releases for app" error

--- provider/aws/service.go
+++ provider/aws/service.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"fmt"
 
+	"github.com/convox/praxis/cache"
 	"github.com/convox/praxis/helpers"
 	"github.com/convox/praxis/types"
 )
@@ -23,6 +24,12 @@ func (p *Provider) ServiceGet(app, name string) (*types.Service, error) {
 }
 
 func (p *Provider) ServiceList(app string) (types.Services, error) {
+	// clear the AppGet cache to avoid "no releases for app"
+	err := cache.Clear("describeStack", fmt.Sprintf("%s-%s", p.Name, app))
+	if err != nil {
+		return nil, err
+	}
+
 	m, _, err := helpers.AppManifest(p, app)
 	if err != nil {
 		return nil, err
