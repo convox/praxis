5479c6946834999b8a3cd7b5962893bfe68fe4db
Noah Zoschke <noah@convox.com>
2017-07-06 17:10:56 -0700

    fix created build logs panic

--- provider/local/build.go
+++ provider/local/build.go
@@ -144,7 +144,7 @@ func (p *Provider) BuildLogs(app, id string) (io.ReadCloser, error) {
 	}
 
 	switch build.Status {
-	case "running":
+	case "created", "running":
 		log.Success()
 		return p.ProcessLogs(app, build.Process, types.LogsOptions{Follow: true, Prefix: false})
 	default:
