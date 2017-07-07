b551f407d20800e13034d116035f5978eea57737
David Dollar <david@dollar.io>
2017-07-07 10:58:04 -0400

    fix router crosstalk

--- provider/local/converge.go
+++ provider/local/converge.go
@@ -1,7 +1,9 @@
 package local
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net/http"
 	"os/exec"
 	"reflect"
 	"strings"
@@ -97,6 +99,26 @@ func (p *Provider) converge(app string) error {
 			return errors.WithStack(log.Error(err))
 		}
 	}
+
+	// TODO: remove
+	dt := http.DefaultTransport.(*http.Transport)
+	dt.TLSClientConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	hc := http.Client{Transport: dt}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/version/%s", p.Router, p.Version), nil)
+	if err != nil {
+		return err
+	}
+
+	res, err := hc.Do(req)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
 
 	return log.Success()
 }
--- router/routes.go
+++ router/routes.go
@@ -3,6 +3,7 @@ package router
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/convox/praxis/api"
 )
@@ -43,4 +44,21 @@ func (rt *Router) ProxyCreate(w http.ResponseWriter, r *http.Request, c *api.Con
 	}
 
 	return c.RenderJSON(p)
+}
+
+func (rt *Router) VersionGet(w http.ResponseWriter, r *http.Request, c *api.Context) error {
+	return c.RenderJSON(map[string]string{
+		"version": rt.Version,
+	})
+}
+
+func (rt *Router) VersionCheck(w http.ResponseWriter, r *http.Request, c *api.Context) error {
+	version := c.Var("version")
+
+	if rt.Version != "dev" && version > rt.Version {
+		fmt.Printf("ns=convox.router at=version client=%s router=%s action=die\n", version, rt.Version)
+		os.Exit(0)
+	}
+
+	return nil
 }
