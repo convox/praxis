17c96263004271d12aafa982c479db6c07540162
Noah Zoschke <noah+sso2@convox.com>
2017-07-10 14:16:23 -0700

    Revert "process run test"
    This reverts commit 988ca5d6bd8d9b73cf17305beca9db47e7ff093f.

--- cmd/qa/process_test.go
+++ cmd/qa/process_test.go
@@ -34,7 +34,7 @@ func TestProcessRun(t *testing.T) {
 		Output:  logs,
 		Service: "web",
 	})
-	assert.EqualError(t, err, "[no release promoted for app: valid]")
+	assert.EqualError(t, err, "[no release for app: valid]")
 	assert.Equal(t, 255, code)
 
 	bs, err := ioutil.ReadAll(logs)
@@ -56,7 +56,7 @@ func TestProcessRun(t *testing.T) {
 		Output:  logs,
 		Service: "web",
 	})
-	assert.EqualError(t, err, "[no release promoted for app: valid]")
+	assert.EqualError(t, err, "[no release for app: valid]")
 	// assert.EqualError(t, err, "[no builds for app: valid]") // FIXME
 	assert.Equal(t, 255, code)
 
@@ -99,137 +99,4 @@ func TestProcessRun(t *testing.T) {
 	out = string(bs)
 	assert.NoError(t, err)
 	assert.Equal(t, "hi\r\n", out)
-}
-
-func TestProcessRunReleases(t *testing.T) {
-	Rack, err := rack.NewFromEnv()
-	assert.NoError(t, err)
-
-	app, err := Rack.AppCreate("valid")
-	defer Rack.AppDelete(app.Name)
-	assert.NoError(t, err)
-
-	r, err := Rack.ReleaseCreate(app.Name, types.ReleaseCreateOptions{
-		Env: map[string]string{
-			"FOO": "bar",
-		},
-	})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, r.Id)
-	r1 := r.Id
-
-	obj, err := Rack.ObjectStore(app.Name, "", tarReader(File{"a.txt", "a"}), types.ObjectStoreOptions{})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, obj.Key)
-
-	b, err := Rack.BuildCreate(app.Name, fmt.Sprintf("object:///%s", obj.Key), types.BuildCreateOptions{})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, b.Id)
-
-	l, err := Rack.BuildLogs(app.Name, b.Id)
-	assert.NoError(t, err)
-	_, err = ioutil.ReadAll(l)
-	assert.NoError(t, err)
-
-	b, err = Rack.BuildGet(app.Name, b.Id)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, b.Release)
-	r2 := b.Release
-
-	r, err = Rack.ReleaseCreate(app.Name, types.ReleaseCreateOptions{
-		Env: map[string]string{
-			"FOO": "baz",
-		},
-	})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, r.Id)
-	r3 := r.Id
-
-	obj, err = Rack.ObjectStore(app.Name, "", tarReader(File{"b.txt", "b"}), types.ObjectStoreOptions{})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, obj.Key)
-
-	b, err = Rack.BuildCreate(app.Name, fmt.Sprintf("object:///%s", obj.Key), types.BuildCreateOptions{})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, b.Id)
-
-	l, err = Rack.BuildLogs(app.Name, b.Id)
-	assert.NoError(t, err)
-	_, err = ioutil.ReadAll(l)
-	assert.NoError(t, err)
-
-	b, err = Rack.BuildGet(app.Name, b.Id)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, b.Release)
-	r4 := b.Release
-
-	logs := bytes.NewBuffer([]byte{})
-	code, err := Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Command: "env ; ls",
-		Output:  logs,
-		Service: "web",
-	})
-	assert.EqualError(t, err, "[no release promoted for app: valid]")
-	assert.Equal(t, 255, code)
-
-	logs = bytes.NewBuffer([]byte{})
-	code, err = Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Command: "env ; ls",
-		Output:  logs,
-		Release: r1,
-		Service: "web",
-	})
-	assert.EqualError(t, err, "[no builds for app: valid]")
-	assert.Equal(t, 255, code)
-
-	bs, err := ioutil.ReadAll(logs)
-	out := string(bs)
-	assert.NoError(t, err)
-	assert.Equal(t, "", out)
-
-	logs = bytes.NewBuffer([]byte{})
-	code, err = Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Command: "env ; ls",
-		Output:  logs,
-		Release: r2,
-		Service: "web",
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, 0, code)
-
-	bs, err = ioutil.ReadAll(logs)
-	out = string(bs)
-	assert.Contains(t, out, "FOO=bar")
-	assert.Contains(t, out, "a.txt")
-
-	logs = bytes.NewBuffer([]byte{})
-	code, err = Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Command: "env ; ls",
-		Output:  logs,
-		Release: r3,
-		Service: "web",
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, 0, code)
-
-	bs, err = ioutil.ReadAll(logs)
-	out = string(bs)
-	assert.Contains(t, out, "FOO=baz")
-	assert.Contains(t, out, "a.txt")
-
-	logs = bytes.NewBuffer([]byte{})
-	code, err = Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Command: "env ; ls",
-		Output:  logs,
-		Release: r4,
-		Service: "web",
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, 0, code)
-
-	bs, err = ioutil.ReadAll(logs)
-	out = string(bs)
-	assert.Contains(t, out, "FOO=baz")
-	assert.Contains(t, out, "b.txt")
-	assert.NotContains(t, out, "a.txt")
 }
