f426661b6815c898c38fafbd549f5e5d960b9166
Noah Zoschke <noah+sso2@convox.com>
2017-07-10 14:16:23 -0700

    Revert "ProcessRun working test"
    This reverts commit 94f268795d5198f23858ddc23d07f8e69f79e0b0.

--- cmd/qa/process_test.go
+++ cmd/qa/process_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"testing"
 
@@ -30,58 +29,9 @@ func TestProcessRun(t *testing.T) {
 	assert.EqualError(t, err, "[no releases for app: valid]")
 	assert.Equal(t, 255, code)
 
-	bs, err := ioutil.ReadAll(logs)
+	bytes, err := ioutil.ReadAll(logs)
 	assert.NoError(t, err)
-	out := string(bs)
+	out := string(bytes)
 
 	assert.Equal(t, "", out)
-
-	r, err := Rack.ReleaseCreate(app.Name, types.ReleaseCreateOptions{
-		Env: map[string]string{
-			"FOO": "bar",
-		},
-	})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, r.Id)
-
-	logs = bytes.NewBuffer([]byte{})
-	code, err = Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Output: logs,
-	})
-	assert.EqualError(t, err, "[no releases for app: valid]")
-	// assert.EqualError(t, err, "[no builds for app: valid]") // FIXME
-	assert.Equal(t, 255, code)
-
-	obj, err := Rack.ObjectStore(app.Name, "", tarReader(), types.ObjectStoreOptions{})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, obj.Key)
-
-	b, err := Rack.BuildCreate(app.Name, fmt.Sprintf("object:///%s", obj.Key), types.BuildCreateOptions{})
-	assert.NoError(t, err)
-	assert.NotEmpty(t, b.Id)
-
-	l, err := Rack.BuildLogs(app.Name, b.Id)
-	assert.NoError(t, err)
-	_, err = ioutil.ReadAll(l)
-	assert.NoError(t, err)
-
-	b, err = Rack.BuildGet(app.Name, b.Id)
-	assert.NoError(t, err)
-
-	err = Rack.ReleasePromote(app.Name, b.Release)
-	assert.NoError(t, err)
-
-	logs = bytes.NewBuffer([]byte{})
-	code, err = Rack.ProcessRun(app.Name, types.ProcessRunOptions{
-		Command: "echo hi",
-		Output:  logs,
-		Service: "web",
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, 0, code)
-
-	bs, err = ioutil.ReadAll(logs)
-	out = string(bs)
-	assert.NoError(t, err)
-	assert.Equal(t, "hi\r\n", out)
 }
