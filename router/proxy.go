845c0db96d549e2443020494d69ab4761d6d369e
Miguel Moll <miguel.moll@gmail.com>
2017-07-18 15:55:07 -0400

    proxy websockets

--- router/proxy.go
+++ router/proxy.go
@@ -16,6 +16,8 @@ import (
 	"github.com/convox/praxis/helpers"
 	"github.com/convox/praxis/sdk/rack"
 	"github.com/convox/praxis/types"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 
 	mrand "math/rand"
 )
@@ -212,14 +214,18 @@ func (p *Proxy) proxyRackHTTP() (http.Handler, error) {
 		return nil, err
 	}
 
-	px := &httputil.ReverseProxy{Director: p.rackDirector}
+	rp := &httputil.ReverseProxy{Director: p.rackDirector}
 
 	switch kind {
 	case "service":
-		px.Transport = logTransport{RoundTripper: serviceTransport(app, service, pi)}
+		rp.Transport = logTransport{RoundTripper: serviceTransport(app, service, pi)}
 	default:
 		return nil, fmt.Errorf("unknown proxy type: %s", kind)
 	}
+
+	px := mux.NewRouter()
+	px.HandleFunc("/{path:.*}", p.ws(app, service, pi)).Methods("GET").Headers("Upgrade", "websocket")
+	px.Handle("/{path:.*}", rp)
 
 	return px, nil
 }
@@ -274,4 +280,88 @@ func serviceProxy(rk rack.Rack, app, pid string, port int, rw io.ReadWriter) err
 	}
 
 	return nil
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func (p *Proxy) ws(app, service string, port int) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		frontend, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Printf("ns=convox.router at=proxy type=ws.upgrader error=%q\n", err)
+			return
+		}
+
+		dialer := &websocket.Dialer{
+			Proxy: http.ProxyFromEnvironment,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		}
+
+		dialer.NetDial = func(network, address string) (net.Conn, error) {
+			r, err := rack.NewFromEnv()
+			if err != nil {
+				return nil, err
+			}
+
+			pss, err := r.ProcessList(app, types.ProcessListOptions{Service: service})
+			if err != nil {
+				return nil, err
+			}
+
+			if len(pss) < 1 {
+				return nil, fmt.Errorf("no processes available for service: %s", service)
+			}
+
+			ps := pss[mrand.Intn(len(pss))]
+
+			a, b := net.Pipe()
+
+			go serviceProxy(r, app, ps.Id, port, a)
+
+			return &nopDeadlineConn{b}, nil
+		}
+
+		r.URL.Host = p.endpoint.Host
+		r.URL.Scheme = "wss"
+
+		headers := http.Header{}
+		headers.Add("X-Forwarded-For", r.RemoteAddr)
+		headers.Add("X-Forwarded-Port", p.Listen.Port())
+		headers.Add("X-Forwarded-Proto", p.Listen.Scheme)
+
+		for k, v := range r.Header {
+			// Websocket headers to skip as they are set by the dialer and duplicates aren't allowed
+			if k == "Upgrade" || k == "Connection" || k == "Sec-Websocket-Key" ||
+				k == "Sec-Websocket-Version" || k == "Sec-Websocket-Extensions" || k == "Sec-Websocket-Protocol" {
+				continue
+			}
+			for _, s := range v {
+				headers.Add(k, s)
+			}
+		}
+
+		backend, _, err := dialer.Dial(r.URL.String(), headers)
+		if err != nil {
+			fmt.Printf("ns=convox.router at=proxy type=ws.dial error=%q\n", err)
+			return
+		}
+
+		errc := make(chan error, 2)
+		cp := func(dst io.Writer, src io.Reader) {
+			_, err := io.Copy(dst, src)
+			errc <- err
+		}
+
+		go cp(frontend.UnderlyingConn(), backend.UnderlyingConn())
+		go cp(backend.UnderlyingConn(), frontend.UnderlyingConn())
+
+		if err := <-errc; err != nil {
+			fmt.Printf("ns=convox.router at=proxy type=ws.cp error=%q\n", err)
+		}
+	}
 }
