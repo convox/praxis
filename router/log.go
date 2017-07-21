8eb48a51dfcecd1142a650cbd38cb390d55cb9ba
David Dollar <david@dollar.io>
2017-07-21 15:45:56 -0700

    fix timing issue with dns

--- router/log.go
+++ router/log.go
@@ -3,6 +3,7 @@ package router
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/convox/praxis/sdk/rack"
 )
@@ -16,4 +17,11 @@ func (t logTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	fmt.Printf("ns=convox.router at=proxy type=http target=%q\n", req.URL)
 
 	return t.RoundTripper.RoundTrip(req)
+}
+
+func logError(err error) {
+	fmt.Printf("err = %+v\n", err)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	}
 }
